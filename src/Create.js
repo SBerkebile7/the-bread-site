60616a53dc92d042e0e26269dc6847282c002151
Spenny Berks <sberkebile@live.com>
2022-01-07 11:57:01 -0500

    work on blog creation js

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,7 @@ import { useHistory } from 'react-router-dom';
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('Shubham');
+    const [author, setAuthor] = useState('Berkebile');
     const [isPending, setIsPending] = useState(false);
     const history = useHistory();
 
@@ -14,6 +14,44 @@ const Create = () => {
 
         setIsPending(true);
 
-        fetch('http://localhost:8000/blogs')
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            setIsPending(false);
+            history.push('/');
+        })
     }
+
+    return (
+        <div className='create'>
+            <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title</label>
+                <input 
+                    type = 'text'
+                    required
+                    value = {title}
+                    onChange = {(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    required
+                    value = {body}
+                    onChange = {(e) => setBody(e.target.value)}
+                />
+                <label>Blog author:</label>
+                <select 
+                    value = {author}
+                    onChange = {(e) => setAuthor(e.target.value)}
+                >
+                    <option value = 'Berkebile'>Berkebile</option>
+                    <option value = 'Test'>Test</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog</button>}
+            </form>
+        </div>
+    );
 }
