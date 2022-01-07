4c33cf87303f46dc8accae4b8fb7b7acbdeb0bf0
Spenny Berks <sberkebile@live.com>
2022-01-07 11:15:26 -0500

    work on blog

---
+++ src/Create.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { useHistory } from 'react-router-dom';
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Shubham');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs')
+    }
+}
