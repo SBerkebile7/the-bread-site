d6ec5b90ba2d73d9f52a3e1406a0be4e8e6389dc
Spenny Berks <sberkebile@live.com>
2022-01-24 07:20:26 -0500

    work on post widget for similar/recent posts

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import Link from 'next/link';
 
-const PostWidget = () => {
+import { getRecentPosts } from '../services';
+
+const PostWidget = ({ categories, slug }) => {
+    const [relatedPosts, setRelatedPosts] = useState([]);
+
+    useEffect(() => {
+        if(slug){
+            getSimilarPosts(category, slug)
+              .then ((result) => setRelatedPosts(result))
+        } else {
+            getRecentPosts(category, slug)
+              .then ((result) => setRelatedPosts(result))
+        }
+    }, [input])
+
     return (
         <div>
             PostWidget
