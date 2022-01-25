8dda3acc0af4687b8469d02b0717c9b73437d28e
Spenny Berks <sberkebile@live.com>
2022-01-25 11:24:51 -0500

    post details work done, work on displaying post details now

--- services/index.js
+++ services/index.js
@@ -37,7 +37,43 @@ export const getPosts = async() => {
     const result = await request(graphqlAPI, query);
 
     return result.postsConnection.edges;
-}
+};
+
+export const getPostDetails = async(slug) => {
+    const query = gql `
+        query getPostDetails($slug: String!) {
+            post(where: { slug: $slug }){
+                author {
+                    bio
+                    name
+                    id
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                content {
+                    raw
+                }
+            }
+        }
+      
+    `
+
+    const result = await request(graphqlAPI, query, { slug });
+
+    return result.post;
+};
 
 export const getRecentPosts = async () => {
     const query = gql`
@@ -59,9 +95,9 @@ export const getRecentPosts = async () => {
     const result = await request(graphqlAPI, query);
 
     return result.posts;
-}
+};
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
         query GetPostDetails($slug: String!, $categories: [String!]) {
             posts(
@@ -78,10 +114,10 @@ export const getSimilarPosts = async () => {
         }
     `
 
-    const result = await request(graphqlAPI, query);
+    const result = await request(graphqlAPI, query, { categories, slug });
 
     return result.posts;
-}
+};
 
 export const getCategories = async () => {
     const query = gql`
@@ -96,4 +132,4 @@ export const getCategories = async () => {
     const result = await request(graphqlAPI, query);
 
     return result.categories;
-}
+};
