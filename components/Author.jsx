d0cca309781943d775c255e72e84a6118e3579cd
Spenny Berks <sberkebile@live.com>
2022-01-26 09:55:15 -0500

    work on comment field and comment submission

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Image from 'next/image';
 
 const Author = ({ author }) => {
     return (
         <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
-            <div className='absolute left-0 right-2 -top-14'>
-                <img
+            <div className='absolute left-0 right-0 -top-14'>
+                <Image
                     alt={author.name}
+                    unoptimized
                     width='100px'
                     height='100px'
                     className='align-middle rounded-full'
