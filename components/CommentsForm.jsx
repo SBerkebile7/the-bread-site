e3abec3f95e6dd56501cf2d28072c64625ad761b
Spenny Berks <sberkebile@live.com>
2022-01-26 11:26:29 -0500

    work on comment submission, need to integrate with graphCMS

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -11,7 +11,27 @@ const CommentsForm = ({ slug }) => {
     const storeDataEl = useRef();
 
     const handleCommentSubmission = () => {
+        setError(false);
 
+        const { value: comment } = commentEl.current;
+        const { value: name } = nameEl.current;
+        const { value: email } = emailEl.current;
+        const { checked: storeData } = storeDataEl.current;
+
+        if(!comment || !name || !email) {
+            setError(true);
+            return;
+        }
+
+        const commentObj = { name, email, comment, slug };
+
+        if(storeData) {
+            localStorage.setItem('name', name);
+            localStorage.setItem('email', email);
+        } else {
+            localStorage.removeItem('name', name);
+            localStorage.removeItem('email', email);
+        }
     };
 
     return (
@@ -37,6 +57,12 @@ const CommentsForm = ({ slug }) => {
                     placeholder='Email'
                     name='email'
                 />
+            </div>
+            <div className='grid grid-cols-1 gap-4 mb-4'>
+                <div>
+                    <input ref={storeDataEl} type='checkbox' id='storeData' name='storeData' value='true' />
+                    <label className='text-gray-500 cursor-pointer ml-2' htmlFor='storeData'>Save my e-mail and name for the next time I comment.</label>
+                </div>
             </div>
             {error && <p className='text-ss text-red-500'>All fields are required</p>}
             <div className='mt-8'>
