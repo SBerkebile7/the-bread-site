2499857cdf44d254732e21e2987b8c7d70b4e71b
Spenny Berks <sberkebile@live.com>
2022-01-28 10:59:22 -0500

    development of FeaturedPostCard comp

---
+++ components/FeaturedPostCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import moment from "moment";
+import Image from "next/dist/client/image";
+import Link from "next/dist/client/link";
+
+const FeaturedPostCard = ({ post })=> {
+
+}
+
+export default FeaturedPostCard;
