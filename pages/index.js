e0a69313025506b65ce34ad5e64f65722a8de9d2
Spenny Berks <sberkebile@live.com>
2022-01-28 11:52:07 -0500

    fixed error with image displaying for featured posts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services';
-import { FeaturedPosts } from '../sections'
+import { FeaturedPosts } from '../sections';
 
 export default function Home({ posts }) {
   return (
