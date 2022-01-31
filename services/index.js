1863305f521660b7f9655501601c3618cd0f0446
Spenny Berks <sberkebile@live.com>
2022-01-31 11:35:40 -0500

    added getCategoryPost

--- services/index.js
+++ services/index.js
@@ -193,8 +193,33 @@ export const getCategoryPost = async (slug) => {
             postsConnection(where: {categories_some: {slug: $slug}}) {
                 edges {
                     cursor
+                    node {
+                        author {
+                            bio
+                            name
+                            id
+                            photo {
+                                url
+                            }
+                        }
+                        createdAt
+                        slug
+                        title
+                        excerpt
+                        featuredImage {
+                            url
+                        }
+                        categories {
+                            name
+                            slug
+                        }
+                    }
                 }
             }
         }
     `;
+
+    const result = await request(graphqlAPI, query, { slug });
+
+    return result.postsConnection.edges;
 }
