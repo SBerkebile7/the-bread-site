d878d323ca00468c0015b260c6482bc023f63e2d
Spenny Berks <sberkebile@live.com>
2022-02-01 09:29:56 -0500

    vercel refactor work

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import { PostCard, Categories, PostWidget } from '../components'
+import Head from 'next/head';
+import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 import { FeaturedPosts } from '../sections';
 
@@ -11,26 +11,25 @@ export default function Home({ posts }) {
         <link rel="icon" href="/breadsite.ico" />
       </Head>
       <FeaturedPosts />
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) =>( <PostCard post={post.node} key={post.title} />  ))}
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) =>( <PostCard post={post.node} key={post.title} />))}
         </div>
-        <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div>
         </div>
       </div>
-
     </div>
   )
-}
+};
 
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
 
   return {
-    props: { posts }
-  }
+    props: { posts },
+  };
 }
