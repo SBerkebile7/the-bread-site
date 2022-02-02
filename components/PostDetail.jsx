6013623a583f429b812d7915bdb85bafc80e3bb3
Spenny Berks <sberkebile@live.com>
2022-02-02 09:53:39 -0500

    vercel refactor

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -19,7 +19,7 @@ const PostDetail = ({ post }) => {
 
     switch (type) {
       case 'heading-three':
-      return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+        return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
       case 'paragraph':
         return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
       case 'heading-four':
