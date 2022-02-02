42ec2117a63cbd8424ca5c29dbbab4ce79ead40d
Spenny Berks <sberkebile@live.com>
2022-02-02 09:42:07 -0500

    work on fixing vercel error

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -71,7 +71,6 @@ const PostDetail = ({ post }) => {
         <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
         {post.content.raw.children.map((typeObj, index) => {
           const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item));
-      
           return getContentFragment(index, children, typeObj, typeObj.type);
         })}
       </div>
